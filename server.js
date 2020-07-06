b99e58eeaf402e513b0a6e2aac4bbb7b62b5181f
Tevin <tevinward7@gmail.com>
2020-07-05 20:52:12 -0400

    test

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://User:password1@ds133162.mlab.com:33162/heroku_3stmqmrd", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+  // useNewUrlParser: true,
+  // useFindAndModify: false
+  useMongoClient: true
 });
 
 // routes
